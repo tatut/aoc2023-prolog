297190f8caf27773ce93b2a80d898339d9f32171
Tatu Tarvainen <tatu.tarvainen@me.com>
2023-12-06 20:51:48 +0200

    Add an even faster quadratic formula method

--- day6.pl
+++ day6.pl
@@ -1,7 +1,6 @@
 :- use_module(library(clpfd)).
-
-testinput([7-9, 15-40, 30-200]).
 input([53-313, 89-1090, 76-1214, 98-1201]).
+input2(53897698-313109012141201).
 
 moved(Duration, ToBeat, PressTime, Moved) :-
     Moved #> ToBeat,
@@ -14,22 +13,27 @@ ways_to_beat(Duration-ToBeat, Ways) :-
     labeling([min(PressMin),max(PressMax)], [PressMin,PressMax]),
     Ways #= PressMax - PressMin + 1.
 
-mult(A,B,C) :- C is A * B.
-
-part1(Ans) :-
+part1(Ans) :- % 5133600
     input(Races),
     maplist(ways_to_beat, Races, Ways),
-    foldl(mult, Ways, 1, Ans).
+    foldl([A,B,C]>>(C #= A * B), Ways, 1, Ans).
 
-% part1(5133600).
-
-% Part2
-
-testinput2(71530-940200).
-input2(53897698-313109012141201).
-
-part2(Ans) :-
+part2(Ans) :- % 40651271
     input2(Duration-ToBeat),
     ways_to_beat(Duration-ToBeat, Ans).
 
-% part2(40651271).
+quadratic(B,C, S1, S2) :-
+    % A is one and omitted
+    S1 is (B - sqrt(B^2 - 4 * C)) / 2,
+    S2 is (B + sqrt(B^2 - 4 * C)) / 2.
+
+ways_to_beat_q(Duration-ToBeat, Ways) :-
+    quadratic(Duration, ToBeat, S1, S2),
+    Ways is floor(S2) - ceil(S1) + 1.
+
+part1q(Ans) :- % 5133600
+    input(Races),
+    maplist(ways_to_beat_q, Races, Ways),
+    foldl([A,B,C]>>(C #= A * B), Ways, 1, Ans).
+
+part2q(Ans) :- input2(I), ways_to_beat_q(I, Ans).
