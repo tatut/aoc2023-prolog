606b139140c7def957a69bfe5067b31da0ff5966
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2023-12-13 12:20:43 +0200

    Use nb_hashtbl as cache for massive speed improvement!

--- day12.pl
+++ day12.pl
@@ -1,5 +1,5 @@
 :- use_module(util).
-:- dynamic config_cache/3.
+:- use_module(library(nb_hashtbl)).
 
 parse_row(Str, Line-Damaged) :-
     split_string(Str, " ", "", [LinePart,DmgPart]),
@@ -51,40 +51,39 @@ damaged_run([C|Line], D, Rest) :- D > 1, \+ working(C),
                                   damaged_run(Line, D1, Rest).
 
 
-config(I,J, _, Solutions) :- config_cache(I,J, Solutions).
-config(I,J, Row, Solutions) :-
-    \+ config_cache(I,J, _),
-    configs(I,J, Row, Solutions),
-    asserta(config_cache(I,J, Solutions)).
 
-configs(_,_,[]-[], 1). % No more line or contiguous, this solution works!
-configs(_,_,[]-[_|_], 0). % some dmgs unused
-configs(_,_,[35|_]-[], 0). % still damaged, but groups used
-configs(_,_,[35|Line]-[D|_], 0) :- \+ damaged_run(Line, D, _).
-configs(I,J,[35|Line]-[D|Dmgs], Solutions) :- % # must be part of damaged
+config(Cache,I,J, Row, Solutions) :-
+    (nb_hashtbl_get(Cache, I-J, Solutions);
+     (configs(Cache, I,J, Row, Solutions),
+      nb_hashtbl_set(Cache, I-J, Solutions))).
+
+configs(_,_,_,[]-[], 1). % No more line or contiguous, this solution works!
+configs(_,_,_,[]-[_|_], 0). % some dmgs unused
+configs(_,_,_,[35|_]-[], 0). % still damaged, but groups used
+configs(_,_,_,[35|Line]-[D|_], 0) :- \+ damaged_run(Line, D, _).
+configs(Cache,I,J,[35|Line]-[D|Dmgs], Solutions) :- % # must be part of damaged
     damaged_run(Line, D, RestOfLine),
     I1 is I + D, J1 is J + 1,
-    config(I1, J1, RestOfLine-Dmgs, Solutions).
-configs(I,J, [63|Line]-Dmgs, Solutions) :- % ?
+    config(Cache, I1, J1, RestOfLine-Dmgs, Solutions).
+configs(Cache,I,J, [63|Line]-Dmgs, Solutions) :- % ?
     %show(encounter_unknown, [63|Line]),
-    config(I,J,[35|Line]-Dmgs, S1), % ? might be a # (35)
+    config(Cache,I,J,[35|Line]-Dmgs, S1), % ? might be a # (35)
     I1 is I + 1,
-    config(I1, J, Line-Dmgs, S2),      % ? might be a ., recurse
+    config(Cache,I1, J, Line-Dmgs, S2),      % ? might be a ., recurse
     Solutions is S1 + S2.
-configs(I,J,[46|Line]-Dmgs, Solutions) :- % .
+configs(Cache,I,J,[46|Line]-Dmgs, Solutions) :- % .
     I1 is I + 1,
-    config(I1, J, Line-Dmgs, Solutions).
+    config(Cache,I1, J, Line-Dmgs, Solutions).
 
 count_possible_configurations2(Line-Dmgs, Solutions) :-
-    retractall(config_cache(_,_,_)),
-    config(0,0,Line-Dmgs, Solutions).
+    empty_nb_hashtbl(Cache),
+    config(Cache, 0,0,Line-Dmgs, Solutions).
 
 part2(Total) :-
     in(Rows),
     foldl([Row,Sum0,Sum1]>>(times5(Row,Row5x),
                             count_possible_configurations2(Row5x, C),
-                            Sum1 is Sum0 + C,
-                            writeln(current_sum(Sum1))), Rows, 0, Total).
+                            Sum1 is Sum0 + C), Rows, 0, Total).
 
 
 % part2(45322533163795).
